e7e217016083c8b1a4f130fdb13d238fad4989b1
Your Name <sjukic1@etf.unsa.ba>
2020-03-11 11:54:21 +0100

    Initial Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label puruka;
+
+    public void reciHello(ActionEvent actionEvent) {
+        puruka.setText("Hello World") ;  //crt+p sve sto treba pozvati
+    }
+}
+
+//alt + shift i kliknemo multikursor
